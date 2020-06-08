f937f0076865a11f4d6fa5c8d1ee90be4b024d6f
APollari <43749895+APollari@users.noreply.github.com>
2020-06-08 16:37:01 +0300

    Updates

---
+++ precache-manifest.f837f1e62f9ca19cecd3487c22d156b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706f8b9f224d0dcb336927ae3ff0ec41",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "d9d9f0b3b1157304e283",
+    "url": "/portfolio-site/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "f90315fec18ddea3961f",
+    "url": "/portfolio-site/static/css/main.2ec49168.chunk.css"
+  },
+  {
+    "revision": "d9d9f0b3b1157304e283",
+    "url": "/portfolio-site/static/js/2.f6a172a7.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio-site/static/js/2.f6a172a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90315fec18ddea3961f",
+    "url": "/portfolio-site/static/js/main.22a53721.chunk.js"
+  },
+  {
+    "revision": "445412cdfc9cbd9c889b",
+    "url": "/portfolio-site/static/js/runtime-main.b7592738.js"
+  }
+]);
